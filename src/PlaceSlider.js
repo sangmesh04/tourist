839c119446a17de9ac00699d88d07c382b5439a7
sangmesh04 <78370367+sangmesh04@users.noreply.github.com>
2022-03-20 14:37:56 +0530

    color theme change of slider in dark mode

--- src/PlaceSlider.js
+++ src/PlaceSlider.js
@@ -81,23 +81,23 @@ const PlaceSlider = () => {
     </div>
     <div className="carousel-inner">
       <div className="carousel-item active">
-        <img src="https://www.holidify.com/images/cmsuploads/compressed/BangalorePalace16_20190904100428_20190904100439.jpg" className="d-block w-80" alt="..." />
+        <img src="https://www.holidify.com/images/cmsuploads/compressed/BangalorePalace16_20190904100428_20190904100439.jpg" alt="..." />
       </div>
       <div className="carousel-item">
-        <img src="https://www.holidify.com/images/compressed/attractions/attr_1552.jpg" className="d-block w-80" alt="..." />
+        <img src="https://www.holidify.com/images/compressed/attractions/attr_1552.jpg" alt="..." />
       </div>
       <div className="carousel-item">
-        <img src="https://www.holidify.com/images/cmsuploads/compressed/BangalorePalace10_20190920110923.jpg" className="d-block w-80" alt="..." />
+        <img src="https://www.holidify.com/images/cmsuploads/compressed/BangalorePalace10_20190920110923.jpg" alt="..." />
       </div>
     </div>
-    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <i className="carousel-control-prev bi bi-caret-left" id="slideNext" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
       <span className="carousel-control-prev-icon" aria-hidden="true"></span>
       <span className="visually-hidden">Previous</span>
-    </button>
-    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    </i>
+    <i className="carousel-control-next bi bi-caret-right" id="slideNext" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
       <span className="carousel-control-next-icon" aria-hidden="true"></span>
       <span className="visually-hidden">Next</span>
-    </button>
+    </i>
   </div>  
         </div>
         </div>
